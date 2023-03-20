4d458d8ae50e628ee61dd7f885c10e9eae767c9e
dutpekmezi <sirakkaya@gmail.com>
2023-03-20 18:14:50 +0300

    Start New Game and Continue

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -9,15 +9,39 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject startButtonText;
+    [SerializeField] GameObject startNewGameButton;
+
     private void Start() 
     {
         SaveService.LoadGame();
+        if (SaveService.saveData.currentLevel != 0)
+        {
+            startButtonText.GetComponent<TextMeshProUGUI>().text = "CONTINUE";
+
+            startNewGameButton.SetActive(true);
+        }
+        else
+        {
+            startButtonText.GetComponent<TextMeshProUGUI>().text = "START";
+
+            startNewGameButton.SetActive(false);
+        }
     }
 
     public void StartButtonOnClick()
     {
         Debug.Log("current lebvel: " + SaveService.saveData.currentLevel);
         SceneManager.LoadScene(SaveService.saveData.currentLevel);
+    }
+
+    public void StartNewGameButtonOnClick()
+    {
+        SaveService.saveData.currentLevel = 0;
+
+        SceneManager.LoadScene(SaveService.saveData.currentLevel);
+
+        SaveService.SaveGame();
     }
 
 }
